ead7a34f125a16ec8f82b09d956c7b32b26aca2b
Nofrisdan <71428026+Nofrisdan@users.noreply.github.com>
2020-09-17 14:15:33 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>BELAJAR HTML KOMUNITAS LINUX</title>
+</head>
+<body>  
+   <table border="1" width="900px" align="center">
+      <tr>
+         <td colspan="2" height="100px" bgcolor="#7FFFD4" valign="bottom"><h3><marquee>Welcome to Komunitas LINUX STTA</marquee></h3>
+             <table border="1">
+               <tr bgcolor="yellow">
+                  <td width="150px" align="center"><a href="latihan2_php/latihan2.php"> Home</a></td>
+                  <td width="150px" align="center"><a href=""> Profil</a></td>
+                  <td width="150px" align="center"><a href=""> About</a></td>
+                  <td width="150px" align="center"><a href=""> Member</a></td>
+                  <td width="150px" align="center"><a href=""> Confirmation</a></td>
+               </tr>
+             </table>
+         </td>
+      </tr>
+      <tr>
+         <td height=400px width="600px" bgcolor="#808080" valign="top">
+              <h3 align="center">PROFIL</h3>
+             <td width="300px" bgcolor="#808080" valign="center">
+                 <table border="1" valign="top" align="center">
+                   <tr bgcolor="yellow">
+                      <td width="150px" align="center">Kontak</td>
+                    </tr>
+                  </table>
+                  <table border="1" valign="middle" align="center">
+                   <tr bgcolor="yellow">
+                      <td width="150px" align="center">Login</td>
+                    </tr>
+                  </table>
+                  <table border="1" valign="bottom" align="center">
+                   <tr bgcolor="yellow">
+                      <td width="150px" align="center">Kategori</td>
+                    </tr>
+                  </table>
+             </td>
+         </td>
+      </tr>
+      <tr>
+         <td  colspan="2" height="50px" bgcolor="#7FFFD4">
+           <p align="center">follow IG @linuxSTTA</p>
+         
+         </td>
+      </tr>
+
+</body>
+
+
+</html>
